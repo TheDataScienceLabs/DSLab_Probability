d01781ca6e1bcde72b79a766ad88a39e6222f48f
field119 <135546326+field119@users.noreply.github.com>
2023-06-05 13:40:02 -0400

    Bug Fix
    Moved sleep time up to right after the temperature reading so the pico has time to record the temperature before it prints

--- book/labs/pico_temperature.py
+++ book/labs/pico_temperature.py
@@ -8,8 +8,8 @@ num = 12
 
 while num>0:
   reading = temp_sensor.read_u16()      # read in sensor value
+  time.sleep(0.25)   
   file.write(str(reading) + "\n")       # "\n" creates a new line in the txt file
-  num -= 1
-  time.sleep(0.25)                      # wait 0.25 seconds before proceeding
+  num -= 1                   # wait 0.25 seconds before proceeding
 
 file.close()
